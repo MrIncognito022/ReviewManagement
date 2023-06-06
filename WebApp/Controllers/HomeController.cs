f109a8461488d663cbeef95b0ac05e645863b738
Ahmed Qureshi <mr.incognito022@gmail.com>
2023-06-06 14:40:43 -0700

    File Uploaded Successfully

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using OfficeOpenXml;
+using WebApp.Data;
 using WebApp.Models;
 
-namespace WebApp.Controllers
+namespace YourNamespace.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostingEnvironment, ApplicationDbContext context)
         {
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,15 +31,56 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
         {
-            return View();
-        }
+            if (file == null || file.Length == 0)
+            {
+                ModelState.AddModelError("File", "Please select an Excel file to upload.");
+                return View("Index");
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (!Path.GetExtension(file.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                ModelState.AddModelError("File", "Please select an Excel file (.xlsx).");
+                return View("Index");
+            }
+
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "uploads", file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                var rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    var firstName = worksheet.Cells[row, 1].Value?.ToString();
+                    var lastName = worksheet.Cells[row, 2].Value?.ToString();
+                    var checkoutDate = worksheet.Cells[row, 3].Value?.ToString();
+                    var email = worksheet.Cells[row, 4].Value?.ToString();
+                    var todayDate = DateTime.Now;
+
+                    var data = new Customer
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        CheckoutDate = DateTime.Parse(checkoutDate),
+                        Email = email,
+                        TodayDate = todayDate
+                    };
+
+                    _context.Customers.Add(data);
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
